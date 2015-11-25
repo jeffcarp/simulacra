f1812b37622e350a4822438cdda636b98b7bf8ad
0x8890 <0x8890@airmail.cc>
2015-11-25 10:37:20 +0100

    Update website from deploy script.

--- simulacra.js
+++ simulacra.js
@@ -1,3 +1,10 @@
+/*!
+ * Simulacra.js
+ * Version 0.1.2
+ * Copyright (c) 2015 0x8890 <0x8890@airmail.cc>
+ * MIT License
+ * https://github.com/0x8890/simulacra
+ */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict'
 
